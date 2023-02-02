360570d3c30337da79742f2128d2d36279fca7d6
myeongju00 <j51677767@gmail.com>
2023-02-02 17:54:57 +0900

    [feat] : 댓글 엔티티 작성

--- src/main/java/com/dblab/webservice/domain/comment/Comment.java
+++ src/main/java/com/dblab/webservice/domain/comment/Comment.java
@@ -1,5 +1,45 @@
 package com.dblab.webservice.domain.comment;
 
-public class Comment {
+import com.dblab.webservice.domain.posts.Posts;
+import com.dblab.webservice.domain.user.User;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+    @ManyToOne(fetch =  FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Posts posts;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name="user_id", nullable = false)
+    private User user;
+
+    private String commentText;
+
+    private Long commentLikeCount;
+
+    private boolean isLiked;
+    
 }
